573153322e5eac89c9a90e48ea7c9d175705cae3
shaowei.song <571648942@qq.com>
2022-10-17 17:14:07 +0800

    merge

---
+++ ae.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PATH=/usr/local/cuda-11.3/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-11.3/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export CUDA_HOME=/usr/local/cuda-11.3
+
+source env_romp/bin/activate
